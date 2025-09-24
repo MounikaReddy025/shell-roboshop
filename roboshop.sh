dc4fc9eeb45a1730d3b17d724d6455a3b350d976
mounika <info@joindevops.com>
2025-09-24 09:33:49 +0530

    session-06

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-01ba20ec749c1a0dc" # replace with your security group id
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances   --image-id ami-09c813fb71547fc4f   --instance-type t3.micro   --security-group-ids sg-01ba20ec749c1a0dc   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Get private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-0446e3d41fbe67c96 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-0446e3d41fbe67c96 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+
+    echo "$instance: $IP"
+done
