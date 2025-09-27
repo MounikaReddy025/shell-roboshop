46108dd2fb8e5ae1ae5ddba7b416ac9e011e7345
mounika <info@joindevops.com>
2025-09-27 10:33:31 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01ba20ec749c1a0dc" # replace with your security group id
+SG_ID="sg-01ba20ec749c1a0dc" # replace with your security group ID
+ZONE_ID="Z00504622LO4Y0MR1CDDC" # replace with your ID
+DOMAIN_NAME="mouni86s.fun"
 
 for instance in $@
 do
@@ -10,9 +12,31 @@ do
     # Get private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.mouni86s.fun
+
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
-    echo "$instance: $IP"
+    
+     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'" $IP "'"
+            }]
+        }
+        }]
+    }
+    '
 done
