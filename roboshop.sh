5d1f1ffd34c4d027b18713324853935a4c5b7ee7
mounika <info@joindevops.com>
2025-10-02 17:25:06 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,7 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
 
-    if
+    fi
 
     echo "$instance: $IP"
 
