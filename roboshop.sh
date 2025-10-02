ab369ec144b55d413613823a45ca2cde2166593c
mounika <info@joindevops.com>
2025-10-02 17:14:19 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -5,15 +5,36 @@ SG_ID= "sg-01ba20ec749c1a0dc" # replace with your SG ID
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01ba20ec749c1a0dc --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "fronted" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-0d19a7ed8b21fd56f --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
-        IP=$(aws ec2 describe-instances --instance-ids i-0d19a7ed8b21fd56f --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
 
     if
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id Z00504622LO4Y0MR1CDDC \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "' $IP '"
+            }]
+        }
+        }]
+    }
+    '
 done
